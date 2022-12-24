91f9e955e91dc406bcfb01a60716366704a081f9
unknown <bhatiaharjot2000@gmail.com>
2022-12-24 23:19:00 +0530

    fifth file

---
+++ 05conditional.js
@@ -0,0 +1,13 @@
+// if else 
+var temp=20;
+if(temp>20){
+console.log("it's not that cold");
+}
+if(temp<=20)
+{
+  console.log("it's cold wear clothes else mom will kill you :)")  ;
+}
+
+// now for conditionls we have || or  && and opearators also 
+// now one tip a*b+c is good for armature way but you know that you should use the like (a+b)*c as the new way looks good plus is a good pratice to follow 
+ 
