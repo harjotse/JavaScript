7c035216da7310054a177a769c6d30052f56cc07
unknown <bhatiaharjot2000@gmail.com>
2022-12-24 19:10:36 +0530

    third file

---
+++ 03userSign.js
@@ -0,0 +1,15 @@
+var fullName="Harjot Singh"
+var age=22;
+var dob = 14_10_2000
+var Spec= "CSE";
+var password= "ilikeCS"
+
+console.log (fullName)
+console.log(age)
+// now this is not a clear way of doing the stuff
+ console.log(`
+ the name is: ${fullName}
+ the age is :${age}
+ Dob is :${dob}
+ this person likes:${Spec}
+` );
